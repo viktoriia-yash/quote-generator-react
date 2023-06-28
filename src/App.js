afc57f0a6e27fbde35a04eb32135ef741f52faf2
viktoriia-yash <yashkina.vic@gmail.com>
2023-06-28 23:37:19 +0300

    wrote basic HTML code structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="advice-quote">
+          <h2></h2>
+        </div>
+        <div className="btn-container">
+          <button>I need advice</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
