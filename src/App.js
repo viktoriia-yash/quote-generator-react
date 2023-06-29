0cf80913bc6f6ce1d312c2100351632893aa993b
viktoriia-yash <yashkina.vic@gmail.com>
2023-06-29 13:40:56 +0300

    built-in API
    Quote's text, quote's author, and button to get a new quote.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState("Hi");
+  const [author, setAuthor] = useState("Hello");
+
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  }, []);
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="advice-quote">
-          <h2></h2>
+          <h2>{quote}</h2>
+          <div>{author}</div>
         </div>
         <div className="btn-container">
-          <button>I need advice</button>
+          <button onClick={fetchNewQuote}>I need advice</button>
         </div>
       </div>
     </div>
